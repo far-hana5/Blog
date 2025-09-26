ff7215e20215539582469e64d8e97d229410dc77
far-hana5 <farhanahussain5730532@gmail.com>
2025-09-26 12:58:05 +0600

    createpost

--- myBlog/postBlog/views.py
+++ myBlog/postBlog/views.py
@@ -1,5 +1,55 @@
 from django.shortcuts import render
+from .models import postBlog
+from .forms import postForm
+from django.shortcuts import get_object_or_404,redirect
+
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+
+def post_list(request):
+    posts=postBlog.objects.all().order_by('-created_at')
+    return render(request,'post_list.html',{'posts':posts})
+
+def post_create(request):
+    if request.method=="POST":
+      form=postForm(request.POST,request.FILES)
+      if form.is_valid():
+            post=form.save(commit=False)
+            post.user=request.user
+            post.save()
+            return redirect('post_list')
+
+            
+
+    else:
+        form=postForm()
+    return render(request,'post_form.html',{'form':form})
+
+
+def post_edit(request,post_id):
+    post=get_object_or_404(postBlog,pk=post_id,user=request.user)
+    if request.method=='POST':
+        form=postForm(request.POST,request.FILES,instance=post)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.user=request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form=postForm(instance=post)
+    return render(request,'post_form.html',{'form':form})
+
+
+
+def post_delete(request,post_id):
+    post=get_object_or_404(postBlog,pk=post_id,user=request.user)
+    if request.method=='POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request,'post_confirm_delete.html',{'post':post})
+    
+    
+     
